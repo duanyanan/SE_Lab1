c0fdd4acf11f2e77eae6d116a918eebbca3d8c56
duanyanan <duanyanan96@outlook.com>
2016-10-12 20:28:57 +0800

    to merge

--- SE_Lab/表达式计算/src/表达式计算/PolynomialaArithmetic.java
+++ SE_Lab/表达式计算/src/表达式计算/PolynomialaArithmetic.java
@@ -1,4 +1,4 @@
-package ±í´ïÊ½¼ÆËã;
+package 表达式计算;
 
 import java.util.*;
 
@@ -10,7 +10,7 @@ public class PolynomialaArithmetic {
 		String Input ;
 		int la=0;
 		Expression a=new Expression();
-		System.out.println("Welcome to the PolynomialaArithmetic monitor.\nServer version: 1.0.9\nCopyright (c) 2016¡£\nType '!h' for help. Type '!q' for exit.\n ");
+		System.out.println("Welcome to the PolynomialaArithmetic monitor.\nServer version: 1.0.9\nCopyright (c) 2016隆拢\nType '!h' for help. Type '!q' for exit.\n ");
 		while(true){
 			Input = lalala.nextLine();
 			try{
@@ -21,13 +21,13 @@ public class PolynomialaArithmetic {
 					break;
 				}
 				else if (Input.equals("!h"))
-					System.out.println("输入表达式\n!化简!simplify a=1  !d/dx \n±ØÐëÑÏ¸ñ°´ÕÕ¹æÔòÊäÈë£¡");
+					System.out.println("杈撳叆琛ㄨ揪寮廫n!鍖栫畝!simplify a=1  !d/dx \n卤脴脨毛脩脧赂帽掳麓脮脮鹿忙脭貌脢盲脠毛拢隆");
 
 				else if(Input.substring(0,4).equals("!d/d")||Input.substring(0,9).equals("!simplify"))
 				{
 					if(la==0)
-						//乱码了。。。
-						System.out.println("ÉÐÎ´ÊäÈëÈÎºÎ±í´ïÊ½£¬ÎÞ·¨Ö´ÐÐÃüÁî¡£\n");
+						//涔辩爜浜嗐�傘�傘��
+						System.out.println("脡脨脦麓脢盲脠毛脠脦潞脦卤铆麓茂脢陆拢卢脦脼路篓脰麓脨脨脙眉脕卯隆拢\n");
 					else 
 						a.Command(Input);
 				}
@@ -42,7 +42,7 @@ public class PolynomialaArithmetic {
 		}
 			catch(Exception e)
 			{
-				System.out.println("ÊäÈë²»ºÏ·¨£¡\n"+e);
+				System.out.println("脢盲脠毛虏禄潞脧路篓拢隆\n"+e);
 			}
 		}
 		lalala.close();
